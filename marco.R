f8b171501d50909c01113645c7f7645732b02349
kaeferbeckMarco <marco.kaeferbeck@gmail.com>
2025-03-28 13:57:31 +0100

    added dml for tumor size

--- marco.R
+++ marco.R
@@ -7,3 +7,36 @@ df <- df %>%
   mutate(across(where(is.character), as.factor))
 
 str(df)
+
+library(mlr3verse)
+library(DoubleML)
+
+library(data.table)
+
+df_dt <- as.data.table(df)
+
+# Then create the DoubleMLData object
+dml_data <- DoubleMLData$new(
+  df_dt,
+  y_col = "Survival_Rate",
+  d_cols = "Tumor_Size",
+  x_cols = c("Age","Gender","Location","Histology","Family_History")
+)
+
+
+
+# Learner for outcome and treatment model
+ml_g <- lrn("regr.ranger", num.trees = 500)  # for outcome model
+ml_m <- lrn("regr.ranger", num.trees = 500)  # for treatment model
+
+# Initialize the DoubleMLPLR (Partially Linear Regression) model
+dml_plr <- DoubleMLPLR$new(data = dml_data,
+                           ml_g = ml_g,
+                           ml_m = ml_m,
+                           n_folds = 5)
+
+# Fit the model
+dml_plr$fit()
+
+# Print summary
+print(dml_plr$summary())
